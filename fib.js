79b7a9d09f32796c43624c653dc2fe13a6b6f05b
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-06-03 07:21:04 +0100

    -[]rename index o fib -[]replaced all the var to let declearation

---
+++ fib.js
@@ -0,0 +1,20 @@
+function fibonacci(n) {
+  if (n <= 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+      sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
+  }
+}
+
+// Testing the fibonacci function
+let n = 10; // Number of Fibonacci numbers to generate
+let result = fibonacci(n);
+console.log(result); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
